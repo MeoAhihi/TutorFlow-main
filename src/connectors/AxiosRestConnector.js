0f9c5bec39615ef6e915a7074d2126d47ad624f5
Luu Khoa Hoc <lkhoc200443@gmail.com>
2025-01-04 14:26:43 +0700

    remove cookies and use localStorage

--- src/connectors/AxiosRestConnector.js
+++ src/connectors/AxiosRestConnector.js
@@ -1,19 +1,18 @@
 import axios from "axios";
-import Cookie from "js-cookie";
 
 import { ACCESS_TOKEN } from "../constants/connector";
 
 /**
  * @type {string}
  */
-const restConnector = (cookie) => {
+const restConnector = () => {
   const CreateRestConnector = axios.create({
     baseURL: import.meta.env.VITE_BASE_API_URL,
     timeout: 30000,
     withCredentials: false,
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${cookie ? cookie : Cookie.get(ACCESS_TOKEN)}`,
+      Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
     },
   });
 
@@ -23,7 +22,7 @@ const restConnector = (cookie) => {
     },
     (error) => {
       if (error.response && error.response.status === 401) {
-        Cookie.remove(ACCESS_TOKEN);
+        localStorage.removeItem(ACCESS_TOKEN);
         window.location.reload();
       }
       return Promise.reject(error);
